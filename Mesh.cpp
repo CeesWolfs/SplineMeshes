26a006f15648ceb0284cc54cf163ba67bc968eda
ayush <S.A.S.patandin@student.tudelft.nl>
2020-11-20 22:00:06 +0100

    Added some structure to the splitXY function.

--- Mesh.cpp
+++ Mesh.cpp
@@ -15,9 +15,11 @@ Mesh::Mesh(/* args */) {
 	vertices.push_back({ 0.0,1.0,1.0 });
     // Add initial 3D cuboid
     cuboids.push_back({0,1,2,3,4,5,6,7});
+
     // Add 6 border faces
     for (int i=0;i<6;i++) {
         F2f.push_back(halfFace(border_id));
+        V2f.push_back({ 0,i });
     }
 }
 
@@ -35,21 +37,74 @@ halfFace Mesh::Twin(const halfFace hf) const {
  * Create 4 new vertices, merge if vertex already exists in neigboring element
  * 
 */
-uint32_t Mesh::SplitAlongXY(uint32_t elem_id, float z_split) {
-    const uint32_t new_elem_id = cuboids.size();
+uint32_t Mesh::SplitAlongXY(uint32_t cuboid_id, float z_split) {
+
+    // border checks
+    if ((z_split <= vertices[cuboids[cuboid_id].v1].z) || z_split >= vertices[cuboids[cuboid_id].v5].z) {
+			return -1; // Splitpoint not in cuboid
+	}
+
+    // All the old vertices
+    const Vertex v1_old = vertices[cuboids[cuboid_id].v1];
+    const Vertex v2_old = vertices[cuboids[cuboid_id].v2];
+    const Vertex v3_old = vertices[cuboids[cuboid_id].v3];
+    const Vertex v4_old = vertices[cuboids[cuboid_id].v4];
+    
     // All the new vertices
-    const Vertex v1_new = { vertices[cuboids[elem_id].v1].x, vertices[cuboids[elem_id].v1].y, z_split };
-    const Vertex v2_new = { vertices[cuboids[elem_id].v2].x, vertices[cuboids[elem_id].v2].y, z_split };
-    const Vertex v3_new = { vertices[cuboids[elem_id].v3].x, vertices[cuboids[elem_id].v3].y, z_split };
-    const Vertex v4_new = { vertices[cuboids[elem_id].v4].x, vertices[cuboids[elem_id].v4].y, z_split };
+    const Vertex v1_new = { v1_old.x, v1_old.y, z_split };
+    const Vertex v2_new = { v2_old.x, v2_old.y, z_split };
+    const Vertex v3_new = { v3_old.x, v3_old.y, z_split };
+    const Vertex v4_new = { v4_old.x, v4_old.y, z_split };
+
+    // if a vertex is not available yet in the verties vector, then add it.
+    bool notcontains = std::find(vertices.begin(), vertices.end(), v1_new) == vertices.end();
+
+    // Update old cuboid vertices
+    uint32_t v1_new_ref = vertices.size();
+    uint32_t v2_new_ref = v1_new_ref + 1;
+    uint32_t v3_new_ref = v1_new_ref + 2;
+    uint32_t v4_new_ref = v1_new_ref + 3;
+
+    if (notcontains) {
+        vertices.push_back(v1_new);
+        vertices.push_back(v2_new);
+        vertices.push_back(v3_new);
+        vertices.push_back(v4_new);
+    } else {
+        return -1;
+    }
+
+    const uint32_t new_cuboid_id = cuboids.size();
+    Cuboid old_cuboid = cuboids[cuboid_id];
+    cuboids.push_back(old_cuboid);
+
+    old_cuboid.v5 = v1_new_ref;
+    old_cuboid.v6 = v2_new_ref;
+    old_cuboid.v7 = v3_new_ref;
+    old_cuboid.v8 = v4_new_ref;
+
+    // Update new cuboid vertices
+    Cuboid new_cuboid = cuboids[new_cuboid_id];
+    new_cuboid.v1 = v1_new_ref;
+    new_cuboid.v2 = v2_new_ref;
+    new_cuboid.v3 = v3_new_ref;
+    new_cuboid.v4 = v4_new_ref;
+
     // For the new cuboid point the lower face to the top face of the old element
-    F2f.push_back(halfFace(elem_id, 1));
+    F2f.push_back(halfFace(cuboid_id, 1));
+
     // For now just push back the twins of the original element
-    F2f.push_back(Twin(halfFace(elem_id, 1)));
-    F2f.push_back(Twin(halfFace(elem_id, 2)));
-    F2f.push_back(Twin(halfFace(elem_id, 3)));
-    F2f.push_back(Twin(halfFace(elem_id, 4)));
-    F2f.push_back(Twin(halfFace(elem_id, 5)));
+    F2f.push_back(Twin(halfFace(cuboid_id, 1)));
+    F2f.push_back(Twin(halfFace(cuboid_id, 2)));
+    F2f.push_back(Twin(halfFace(cuboid_id, 3)));
+    F2f.push_back(Twin(halfFace(cuboid_id, 4)));
+    F2f.push_back(Twin(halfFace(cuboid_id, 5)));
+
+    // TODO: Update V2f half faces 
+
+    // TODO: Update the twin faces with the correct vertices.
+
+    return new_cuboid_id;
 }
 
 /**
@@ -58,13 +113,20 @@ uint32_t Mesh::SplitAlongXY(uint32_t elem_id, float z_split) {
  * Create 4 new vertices, merge if vertex already exists in neigboring element.
  * 
 */
-uint32_t Mesh::SplitAlongYZ(uint32_t elem_id, float x_split) {
-    const uint32_t new_elem_id = cuboids.size();
+uint32_t Mesh::SplitAlongYZ(uint32_t cuboid_id, float x_split) {
+    const uint32_t new_cuboid_id = cuboids.size();
+    
+    // All the old vertices
+    const Vertex v1_old = vertices[cuboids[cuboid_id].v1];
+    const Vertex v2_old = vertices[cuboids[cuboid_id].v2];
+    const Vertex v3_old = vertices[cuboids[cuboid_id].v3];
+    const Vertex v4_old = vertices[cuboids[cuboid_id].v4];
+
     // All the new vertices
-    const Vertex v1_new = { vertices[cuboids[elem_id].v1].x, vertices[cuboids[elem_id].v1].y, x_split };
-    const Vertex v2_new = { vertices[cuboids[elem_id].v2].x, vertices[cuboids[elem_id].v2].y, x_split };
-    const Vertex v3_new = { vertices[cuboids[elem_id].v3].x, vertices[cuboids[elem_id].v3].y, x_split };
-    const Vertex v4_new = { vertices[cuboids[elem_id].v4].x, vertices[cuboids[elem_id].v4].y, x_split };
+    const Vertex v1_new = { x_split, v1_old.y, v1_old.z };
+    const Vertex v2_new = { x_split, v2_old.y, v2_old.z };
+    const Vertex v3_new = { x_split, v3_old.y, v3_old.z };
+    const Vertex v4_new = { x_split, v4_old.y, v4_old.z };
 
     // TODO:
 }
@@ -76,6 +138,20 @@ uint32_t Mesh::SplitAlongYZ(uint32_t elem_id, float x_split) {
  * Create 4 new vertices, merge if vertex already exists in neigboring element.
  * 
 */
-uint32_t Mesh::SplitAlongXZ(uint32_t elem_id, float y_split) {
-    // TODO
+uint32_t Mesh::SplitAlongXZ(uint32_t cuboid_id, float y_split) {
+    const uint32_t new_cuboid_id = cuboids.size();
+    
+    // All the old vertices
+    const Vertex v1_old = vertices[cuboids[cuboid_id].v1];
+    const Vertex v2_old = vertices[cuboids[cuboid_id].v2];
+    const Vertex v3_old = vertices[cuboids[cuboid_id].v3];
+    const Vertex v4_old = vertices[cuboids[cuboid_id].v4];
+
+    // All the new vertices
+    const Vertex v1_new = { v1_old.x, y_split, v1_old.z };
+    const Vertex v2_new = { v2_old.x, y_split, v2_old.z };
+    const Vertex v3_new = { v3_old.x, y_split, v3_old.z };
+    const Vertex v4_new = { v4_old.x, y_split, v4_old.z };
+
+    // TODO:
 }
--- Mesh.hpp
+++ Mesh.hpp
@@ -6,6 +6,7 @@
 #include <cstdio>
 #include <string>
 #include <fstream>
+#include <algorithm>
 #include "Types.hpp"
 
 class Mesh
@@ -16,12 +17,13 @@ private:
     std::vector<Cuboid> cuboids;
     // Stores a mapping of Half faces to twin half faces
     std::vector<halfFace> F2f;
+    std::vector<halfFace> V2f;
 
 public:
     halfFace Twin(const halfFace hf) const;
-    uint32_t SplitAlongXY(uint32_t elem_id, float z_split);
-    uint32_t SplitAlongYZ(uint32_t elem_id, float x_split);
-    uint32_t SplitAlongXZ(uint32_t elem_id, float y_split);
+    uint32_t SplitAlongXY(uint32_t cuboid_id, float z_split);
+    uint32_t SplitAlongYZ(uint32_t cuboid_id, float x_split);
+    uint32_t SplitAlongXZ(uint32_t cuboid_id, float y_split);
     Mesh(/* args */);
     ~Mesh();
 };
