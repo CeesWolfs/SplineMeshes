5dd6f3f45bbe2241df34c239c7c1a10bd940fb3b
CeesWolfs <ceeswolfs1985@gmail.com>
2020-11-23 14:00:04 +0100

    Deleted some files

--- unittests/run_coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-make clean
-make cov
-rm -rf coverage/
-./test_cov
-ls *.cpp | grep -v "main-test.cpp" | xarg {} gcov {}
-lcov -c --directory . --output-file coverage.info
-genhtml coverage.info --output coverage
-firefox coverage/index.html
