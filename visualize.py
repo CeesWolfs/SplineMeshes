abc1d7cbab53abfbe1f3b08a93ec3a0d76372817
ayush <S.A.S.patandin@student.tudelft.nl>
2020-12-07 18:33:28 +0100

    added labels to vertices in visualize.py

--- visualize.py
+++ visualize.py
@@ -9,7 +9,10 @@ p.add_mesh(mesh, show_edges=True, color=None, opacity=0.85)
 p.add_mesh(mesh.extract_cells(0),
            color='pink', edge_color='blue',
            line_width=5, show_edges=True)
-p.add_mesh(pv.PolyData(mesh.points), color='red',
+poly = pv.PolyData(mesh.points)
+poly["My Labels"] = [f"{i}" for i in range(poly.n_points)]
+p.add_mesh(poly, color='red',
        point_size=10, render_points_as_spheres=True)
+p.add_point_labels(poly, "My Labels", font_size=30)
 p.show_axes()
 p.show()
